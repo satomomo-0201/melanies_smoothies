f74430f9ce202e4b4e057b5850a4aa4757b51030
satomomo-0201 <sato.momo.5446@members.co.jp>
2025-12-02 15:22:28 +0900

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app
    （Custom Smoothies SiSアプリからコピーしました）

---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+# Custom Smoothie Order Form & Pending Orders
+
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import when_matched
+
+# Snowparkセッションの取得
+session = get_active_session()
+
+# --- 注文フォーム（Custom Smoothie Order Form）のロジック ---
+
+# アプリのタイトル
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+# データベースから利用可能なフルーツのオプションを取得
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).collect()
+
+# ingredients_list (st.multiselectはリストを期待するため、my_dataframeをリストに変換)
+ingredients_list = st.multiselect(
+'Choose up to 5 ingredients:'
+, [row[0] for row in my_dataframe] 
+, max_selections=5
+)
+
+time_to_insert = st.button('Submit Order')
+
+if ingredients_list and time_to_insert:
+    # 選択されたフルーツをカンマ区切りの文字列に変換
+    ingredients_string = ', '.join(ingredients_list)
+
+    # 挿入ステートメントを f-string で作成
+    my_insert_stmt = f"insert into smoothies.public.orders(ingredients, name_on_order) values ('{ingredients_string}', '{name_on_order}')"
+    
+    # データをSnowflakeに挿入
+    session.sql(my_insert_stmt).collect()
+    
+    # 成功メッセージ
+    st.success(f'{name_on_order}, your Smoothie is ordered!', icon="✅")
